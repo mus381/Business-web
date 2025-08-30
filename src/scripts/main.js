9ba6404abb9879bc9616ccdda6bd415158b5936e
g-n-musa-latitude-E7440 <m82515661@gmail.com>
2025-08-30 20:14:14 +0300

    removed main.js from scripts

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// src/main.js
-import './styles/base/reset.css'
-import './styles/base/variables.css'
-import './styles/base/base.css'
-import './styles/base/main.css'
-
-// JS entry
-import './scripts/app.js'
