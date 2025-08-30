1127fba47d5a2270e8c8408740604394d7a26e74
g-n-musa-latitude-E7440 <m82515661@gmail.com>
2025-08-30 19:41:57 +0300

    updated base and scripts folders in src

---
+++ src/scripts/app.js
@@ -0,0 +1,2 @@
+// app.js
+console.log('Day 2 Vite setup is working ✅')
---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+// src/main.js
+import './styles/base/reset.css'
+import './styles/base/variables.css'
+import './styles/base/base.css'
+import './styles/base/main.css'
+
+// JS entry
+import './scripts/app.js'
